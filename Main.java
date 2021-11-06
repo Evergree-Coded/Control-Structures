d98d4cb78f9aefabca2ed3bdf4629bf0a081d799
EvergreenSchool <92188421+Evergree-Coded@users.noreply.github.com>
2021-11-06 00:05:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,46 @@
+class Main {
+  public static void main(String[] args) {
+    //int x = 8;
+    /*if (x == 5){
+      System.out.println("x == 5");
+      System.out.println("correct!");
+    } 
+    else{
+      System.out.println("x is not 5");
+    }*/
+
+
+    /*int age = 14;
+
+    if(age > 20){
+      System.out.println("You are old!");
+    }
+    else if(age < 10){
+      System.out.println("Wow!");
+    }
+    else if(age == 16){
+      System.out.println("You are 16!");
+    }
+    else{
+      System.out.println("You are young!");
+    }*/
+    int x = 5;
+    if(x != 3){
+      System.out.println("true");
+    }
+    if(x > 2){
+      System.out.println("big");
+    }
+    if(x < 3){
+      System.out.println("small");
+    }
+    if(x <= 2){
+      System.out.println("nice!");
+    }
+    if(x >= 2){
+      System.out.println("cool!");
+    }
+    
+
+  }
+}
